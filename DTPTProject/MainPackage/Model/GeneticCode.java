8acd50a8bc8a24194ec643ca665e575f932311ab
Pedro Henrique Valente Guths <guthspedro@gmail.com>
2026-03-13 14:40:40 -0300

    Creating a basic hashmap

--- DTPTProject/MainPackage/Model/GeneticCode.java
+++ DTPTProject/MainPackage/Model/GeneticCode.java
@@ -1,5 +1,41 @@
 package DTPTProject.MainPackage.Model;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class GeneticCode {
 
+    private static Map<String, String> codonTable = new HashMap<>();
+
+    static {
+
+        //Start Codon
+        codonTable.put("AUG", "Metionina (Início)");
+        
+        //Stop Codons
+        codonTable.put("UAA", "STOP");
+        codonTable.put("UAG", "STOP");
+        codonTable.put("UGA", "STOP");
+        
+        // Phenilalanina
+        codonTable.put("UUU", "Phe");
+        codonTable.put("UUC", "Phe");
+
+        // Leucina
+        codonTable.put("UUA", "Leu");
+        codonTable.put("UUG", "Leu");
+
+        // Valina
+        codonTable.put("GUU", "Val");
+        codonTable.put("GUC", "Val");
+
+        // Alanina
+        codonTable.put("GCU", "Ala");
+        codonTable.put("GCC", "Ala");
+
+    }
+
+    public static String getAminoAcid(String codon) {
+    return codonTable.getOrDefault(codon, "Inexistent Codon");
+}
 }
