b17f71202d5cd159632814e65fa0628d8a48abf8
Pedro Henrique Valente Guths <guthspedro@gmail.com>
2026-07-21 12:21:51 -0300

    feat: refactoring to spring boot

---
+++ src/main/java/com/pedroguths/dna2proteintranslator/Dna2ProteinTranslatorApplication.java
@@ -0,0 +1,13 @@
+package com.pedroguths.dna2proteintranslator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dna2ProteinTranslatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Dna2ProteinTranslatorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pedroguths/dna2proteintranslator/Dna2ProteinTranslatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pedroguths.dna2proteintranslator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Dna2ProteinTranslatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
